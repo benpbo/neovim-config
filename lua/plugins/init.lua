634e69abf85654440c25d880fdb8c6f8417bd5b7
Ben Prize Bar-On <prizeben@gmail.com>
2025-04-19 19:18:20 +0200

    Put all plugins in one file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,7 @@
-return {
+local overrides = require "configs.overrides"
+
+---@type NvPluginSpec[]
+local plugins = {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
@@ -13,13 +16,103 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "nvchad.configs.lspconfig"
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason,
+  },
+
+  {
+    "Decodetalkers/csharpls-extended-lsp.nvim",
+    ft = "cs",
+  },
+
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function()
+      return require "configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
+    end,
+  },
+
+  {
+    "saecki/crates.nvim",
+    ft = { "rust", "toml" },
+    config = function(_, opts)
+      local crates = require "crates"
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "nvchad.configs.cmp"
+      table.insert(M.sources, { name = "crates" })
+      return M
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "nat-418/scamp.nvim",
+    event = "BufEnter",
+  },
+
+  {
+    "lukoshkin/highlight-whitespace",
+    config = true,
+    event = "BufEnter",
+  },
 }
+
+return plugins
