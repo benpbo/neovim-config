e5c7009c64b7c694c8943d7268f3724917cdea9d
Ben Prize Bar-On <prizeben@gmail.com>
2025-12-14 13:49:25 +0100

    feat(deps): update plugins

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,30 +4,23 @@ local on_init = configs.on_init
 local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "ts_ls", "buf_ls", "clangd", "svelte" }
+local lspconfig = vim.lsp.config
+local servers = { "html", "cssls", "ts_ls", "buf_ls", "clangd", "svelte", "csharp_ls" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  lspconfig[lsp] = {
     on_init = on_init,
     on_attach = on_attach,
     capabilities = capabilities,
   }
 end
 
-lspconfig.pyright.setup {
+lspconfig['pyright'] = {
   on_init = on_init,
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = { "python" },
 }
 
-lspconfig.csharp_ls.setup {
-  on_init = on_init,
-  on_attach = on_attach,
-  capabilities = capabilities,
-  handlers = {
-    ["textDocument/definition"] = require("csharpls_extended").handler,
-    ["textDocument/typeDefinition"] = require("csharpls_extended").handler,
-  },
-}
+vim.lsp.handlers["textDocument/definition"] = require("csharpls_extended").handler
+vim.lsp.handlers["textDocument/typeDefinition"] = require("csharpls_extended").handler
